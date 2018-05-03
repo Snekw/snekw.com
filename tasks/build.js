ee35c93fa6c161a44960f7da46172a7625150e06
snekw <dev.snekw@gmail.com>
2018-05-04 00:47:18 +0300

    Moved copyFile and copyDirectory to utility functions. Added documentation for those functions.

--- tasks/build.js
+++ tasks/build.js
@@ -85,6 +85,65 @@ function clean (dir) {
   }
 }
 
+/**
+ * Create a copy of a file.
+ * @param source Path to the file to be copied.
+ * @param target Path to where to copy the file.
+ * @returns {Promise<any>} Error code or null.
+ */
+function copyFile (source, target) {
+  return new Promise((resolve, reject) => {
+    ensureDir(path.dirname(target), err => {
+      if (err && err.code !== 'EEXIST') {
+        return reject(err);
+      }
+
+      let rd = fs.createReadStream(source);
+      rd.on('error', function (err) {
+        return reject(err);
+      });
+      let wr = fs.createWriteStream(target);
+      wr.on('error', function (err) {
+        return reject(err);
+      });
+      wr.on('close', function (ex) {
+        return resolve();
+      });
+      rd.pipe(wr);
+    });
+  });
+}
+
+/**
+ * Create a copy of a directory
+ * @param source Path to the directory to copy.
+ * @param target Path to the destination directory.
+ * @returns {Promise<any>} Error code or null.
+ */
+function copyDir (source, target) {
+  return new Promise((resolve, reject) => {
+    let promises = [];
+    fs.readdir(source, (err, items) => {
+      if (err) {
+        return reject(err);
+      }
+      for (let i = 0; i < items.length; i++) {
+        let stat = fs.statSync(path.join(source, items[i]));
+        if (stat.isFile()) {
+          promises.push(copyFile(path.join(source, items[i]), path.join(target, items[i])));
+        } else if (stat.isDirectory()) {
+          promises.push(copyDir(path.join(source, items[i]), path.join(target, items[i])));
+        }
+      }
+      Promise.all(promises).then(() => {
+        return resolve();
+      }).catch(err => {
+        return reject(err);
+      });
+    });
+  });
+}
+
 // Build functions
 
 function compileScss (file, style) {
@@ -136,53 +195,6 @@ function saveCssMin (input) {
         } else {
           return resolve('./dist/static/css/' + input.file + '.css written.');
         }
-      });
-    });
-  });
-}
-
-function copyFile (source, target) {
-  return new Promise((resolve, reject) => {
-    ensureDir(path.dirname(target), err => {
-      if (err && err.code !== 'EEXIST') {
-        return reject(err);
-      }
-
-      let rd = fs.createReadStream(source);
-      rd.on('error', function (err) {
-        return reject(err);
-      });
-      let wr = fs.createWriteStream(target);
-      wr.on('error', function (err) {
-        return reject(err);
-      });
-      wr.on('close', function (ex) {
-        return resolve();
-      });
-      rd.pipe(wr);
-    });
-  });
-}
-
-function copyDir (source, target) {
-  return new Promise((resolve, reject) => {
-    let promises = [];
-    fs.readdir(source, (err, items) => {
-      if (err) {
-        return reject(err);
-      }
-      for (let i = 0; i < items.length; i++) {
-        let stat = fs.statSync(path.join(source, items[i]));
-        if (stat.isFile()) {
-          promises.push(copyFile(path.join(source, items[i]), path.join(target, items[i])));
-        } else if (stat.isDirectory()) {
-          promises.push(copyDir(path.join(source, items[i]), path.join(target, items[i])));
-        }
-      }
-      Promise.all(promises).then(() => {
-        return resolve();
-      }).catch(err => {
-        return reject(err);
       });
     });
   });
